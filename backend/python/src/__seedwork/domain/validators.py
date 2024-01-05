0720ac2eef867d6ef9240a6587d2a7b29b6d2488
André Junior <andrejunior179@gmail.com>
2024-01-05 13:32:02 -0300

    feat: :sparkles: add DRF validator

--- backend/python/src/__seedwork/domain/validators.py
+++ backend/python/src/__seedwork/domain/validators.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import Any, Dict, Generic, List, TypeVar
 from abc import ABC, abstractmethod
+from rest_framework.serializers import Serializer
 
 from .exceptions import ValidationException
 
@@ -50,3 +51,20 @@ class ValidatorFieldsInterface(ABC, Generic[PropsValidated]):
     @abstractmethod
     def validate(self, data: Any) -> bool:
         raise NotImplementedError()
+
+
+# Django
+class DRFValidator(ValidatorFieldsInterface[PropsValidated], ABC):
+
+    def validate(self, data: Serializer) -> bool:
+        serializer = data
+
+        if serializer.is_valid():
+            self.validated_data = serializer.validated_data
+            return True
+
+        self.errors = {
+            field: [str(_error) for _error in _errors]
+            for field, _errors in serializer.errors.items()
+        }
+        return False
