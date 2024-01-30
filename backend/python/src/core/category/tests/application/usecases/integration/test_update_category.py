d780a4702bc057cacaf4c461a1eacca9a298c59a
André Junior <andrejunior179@gmail.com>
2024-01-30 20:08:56 -0300

    feat: :sparkles: add lista categories usecase

---
+++ backend/python/src/core/category/tests/application/usecases/integration/test_update_category.py
@@ -0,0 +1,25 @@
+from src.core.category.domain.category import Category
+from src.core.category.application.usecases.update_category import UpdateCategory, UpdateCategoryRequest
+from src.core.category.infra.repositories.in_memory_category_repository import InMemoryCategoryRepository
+
+
+class TestUpdateCategory:
+    def test_can_update_category_name_and_description(self):
+        category = Category(
+            name="Film",
+            description="some description",
+        )
+        repository = InMemoryCategoryRepository()
+        repository.save(category=category)
+
+        use_case = UpdateCategory(repository=repository)
+        request = UpdateCategoryRequest(
+            id=category.id,
+            name="Serie",
+            description="some description 1"
+        )
+        use_case.execute(request=request)
+
+        updated_category = repository.get_by_id(category.id)
+        assert updated_category.name == "Serie"
+        assert updated_category.description == "some description 1"
---
+++ backend/python/src/core/category/tests/application/usecases/unit/test_list_categories.py
@@ -0,0 +1,44 @@
+from unittest.mock import create_autospec
+from src.core.category.application.category_repository import CategoryRepository
+from src.core.category.application.usecases.list_categories import CategoryOutput, ListCategories, ListCategoriesRequest, ListCategoriesResponse
+from src.core.category.domain.category import Category
+
+
+class TestListCategories:
+    def test_when_no_categories_should_return_empty_list(self):
+        mock_repository = create_autospec(CategoryRepository)
+        mock_repository.list.return_value = []
+
+        use_case = ListCategories(repository=mock_repository)
+        request = ListCategoriesRequest()
+
+        response = use_case.execute(request)
+
+        assert response == ListCategoriesResponse(
+            data=[]
+        )
+
+    def test_when_no_categories_should_return_list(self):
+        category = Category(
+            name="Filme",
+            description="Categoria para filmes",
+        )
+
+        mock_repository = create_autospec(CategoryRepository)
+        mock_repository.list.return_value = [category]
+
+        use_case = ListCategories(repository=mock_repository)
+        request = ListCategoriesRequest()
+
+        response = use_case.execute(request)
+
+        assert response == ListCategoriesResponse(
+            data=[
+                CategoryOutput(
+                    id=category.id,
+                    name=category.name,
+                    description=category.description,
+                    is_active=category.is_active,
+                )
+            ]
+        )
