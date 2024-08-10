c8919ed7571bc4cd062cdec3e503b2f2469a9e0b
André Junior <andrejunior179@gmail.com>
2024-08-10 11:13:05 -0300

    feat: :sparkles: add value object uuid

---
+++ backend/typescript/micro-videos-admin/src/shared/domain/value-objects/uuid.vo.ts
@@ -0,0 +1,31 @@
+import {v4 as uuid, validate as uuidValidate} from 'uuid';
+
+import {ValueObject} from '../value-object';
+
+export class Uuid extends ValueObject {
+  readonly id: string;
+
+  constructor(id?: string) {
+    super();
+    this.id = id || this.generate();
+    this.validate();
+  }
+
+  generate(): string {
+    return uuid();
+  }
+
+  private validate() {
+    const isValidateUuid = uuidValidate(this.id);
+    if (!isValidateUuid) {
+      throw new InvalidUuidError();
+    }
+  }
+}
+
+export class InvalidUuidError extends Error {
+  constructor(message: string = 'ID must be a valid UUID') {
+    super(message);
+    this.name = 'InvalidUuidError';
+  }
+}
