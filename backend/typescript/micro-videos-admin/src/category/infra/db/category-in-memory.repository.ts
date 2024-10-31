7c4538fa3c3875224784e1dcfb7d022f3bf313d9
André Junior <andrejunior179@gmail.com>
2024-10-31 11:47:31 -0300

    feat: :sparkles: add category-sequelize and tests

--- backend/typescript/micro-videos-admin/src/category/infra/db/category-in-memory.repository.ts
+++ backend/typescript/micro-videos-admin/src/category/infra/db/category-in-memory.repository.ts
@@ -1,12 +1,13 @@
 import {Category} from '@/category/domain/category.entity';
+import type {ICategoryRepository} from '@/category/domain/category.repository';
 import type {SortDirection} from '@/shared/domain/repository/search-params';
 import {Uuid} from '@/shared/domain/value-objects/uuid.vo';
 import {InMemorySearchableRepository} from '@/shared/infra/db/in-memory/in-memory.repository';
 
-export class CategoryInMemoryRepository extends InMemorySearchableRepository<
-  Category,
-  Uuid
-> {
+export class CategoryInMemoryRepository
+  extends InMemorySearchableRepository<Category, Uuid>
+  implements ICategoryRepository
+{
   sortableFields: string[] = ['name', 'created_at'];
 
   getEntity(): new (...args: any[]) => Category {
